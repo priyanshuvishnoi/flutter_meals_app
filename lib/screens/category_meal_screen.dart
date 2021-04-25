8e3d8a6631f7501a4a690d219b4c8c72e5720287
Priyanshu Vishnoi <priyanshuvishnoi990@gmail.com>
2021-04-25 23:39:17 +0530

    Meals Screen Complete

---
+++ lib/screens/category_meal_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/widgets/meal_item.dart';
+import '../dummy_data.dart';
+
+class CategoryMealScreen extends StatelessWidget {
+  static const routeName = "/categoryMealScreen";
+  // final String categoryId;
+  // final String categoryTitle;
+
+  // CategoryMealScreen(this.categoryId, this.categoryTitle);
+
+  @override
+  Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+
+    final categoryTitle = routeArgs['title'];
+    final categoryId = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS
+        .where((meal) => meal.categories.contains(categoryId))
+        .toList();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(categoryTitle),
+      ),
+      body: ListView.builder(
+        itemCount: categoryMeals.length,
+        itemBuilder: (ctx, index) {
+          return MealItem(
+            affordability: categoryMeals[index].affordability,
+            duration: categoryMeals[index].duration,
+            complexity: categoryMeals[index].complexity,
+            imageUrl: categoryMeals[index].imageUrl,
+            title: categoryMeals[index].title,
+          );
+        },
+      ),
+    );
+  }
+}
